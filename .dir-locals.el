71adca5b07495d6e9b954d187ccb4fac1b7f8fd8
Mark Stuart <codeasone@gmail.com>
2023-05-13 15:36:16 +0100

    Refactor when debugging cljs REPL jack-in from cljs file
    Seems like the -A:dev aliases only register when jacking in from a clj file

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,7 @@
          (cider-preferred-build-tool . clojure-cli)
          (cider-default-cljs-repl . custom)
          (cider-custom-cljs-repl-init-form . "(do (user/cljs-repl))")
-         (cider-clojure-cli-aliases . ":dev")
+         (cider-clojure-cli-aliases . "-A:dev")
          (eval . (progn
                    (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
                    (add-to-list 'cider-jack-in-nrepl-middlewares "shadow.cljs.devtools.server.nrepl/middleware"))))))
--- dev/user.clj
+++ dev/user.clj
@@ -2,10 +2,8 @@
   (:require [shadow.cljs.devtools.api :as shadow]
             [shadow.cljs.devtools.server :as server]))
 
-(defn cljs-repl
-  ([]
-   (cljs-repl :app))
-  ([build-id]
-   (server/start!)
-   (shadow/watch build-id)
-   (shadow/nrepl-select build-id)))
+(def cljs-repl
+  (fn []
+    (server/start!)
+    (shadow/watch :app)
+    (shadow/nrepl-select :app)))

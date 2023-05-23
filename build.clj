299b4447c9f42863faab4fd1df754f5a3acf6199
Mark Stuart <codeasone@gmail.com>
2023-05-23 20:54:49 +0100

    Add scm details to generated pom.xml

--- build.clj
+++ build.clj
@@ -16,7 +16,8 @@
                 :lib lib
                 :version version
                 :basis basis
-                :src-dirs ["src"]})
+                :src-dirs ["src"]
+                :scm {:url "https://github.com/codeasone/tetris-clj"}})
   (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
